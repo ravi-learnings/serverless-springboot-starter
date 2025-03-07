5ba9381295d74ecfa2ba141b3f03a4b610b9b8aa
chravi414 <chravindra414@gmail.com>
2025-03-06 23:24:24 -0600

    Create DynamoDBClient and EnhancedClient beans

--- src/main/java/com/learnings/serverless_springboot/config/DynamoDBConfig.java
+++ src/main/java/com/learnings/serverless_springboot/config/DynamoDBConfig.java
@@ -2,6 +2,9 @@ package com.learnings.serverless_springboot.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 @Configuration
 public class DynamoDBConfig {
@@ -9,5 +12,10 @@ public class DynamoDBConfig {
     @Bean
     public DynamoDbClient dynamoDbClient() {
         return DynamoDbClient.builder().region(Region.US_EAST_1).build();
+    }
+
+    @Bean
+    public DynamoDbEnhancedClient dynamoDbEnhancedClient() {
+        return DynamoDbEnhancedClient.builder().dynamoDbClient(dynamoDbClient()).build();
     }
 }
